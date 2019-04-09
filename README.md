e6f7a6eda14ade8bc8a3212454b7bed838848db8
Philippe Recto <precto1285@gmal.com>
2019-04-08 20:37:35 -0700

    added variables

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # BlackJackJS
-A Black Jack Game built with ES6, HTML &amp; CSS (BOOTSTRAP4)
+A Black Jack Game built with ES6, HTML5 &amp; CSS3 (BOOTSTRAP4)
+
+
+Rules: The user starts with $100 in the bank. THe user is asked by the dealer to place your wager. 
+
+Five (5) choices are available to wager with: 
+1. Wager $1
+2. Wager $5
+3. Wager: $10
+4. Wager $20
+5. Wager $50
---
+++ script.js
@@ -0,0 +1,84 @@
+//Buttons
+var startGame = '';
+var bet5 = '';
+var bet10 = '';
+var bet15 = '';
+var bet20 = '';
+var stay = '';
+var call = '';
+
+//Cards Spades
+var spade = [
+  { card: 'ace', val: [1, 11] },
+  { card: 'two', val: 2 },
+  { card: 'three', val: 3 },
+  { card: 'four', val: 4 },
+  { card: 'five', val: 5 },
+  { card: 'six', val: 6 },
+  { card: 'seven', val: 7 },
+  { card: 'eight', val: 8 },
+  { card: 'nine', val: 8 },
+  { card: 'ten', val: 10 },
+  { card: 'jack', val: 10 },
+  { card: 'queen', val: 10 },
+  { card: 'king', val: 10 },
+];
+
+console.log('ace equals: ' + spade[0].val[1] + ' and ' + spade[0].val[0]);
+console.log();
+
+//Cards Clubs
+var club = [
+  { card: 'ace', val: [1, 11] },
+  { card: 'two', val: 2 },
+  { card: 'three', val: 3 },
+  { card: 'four', val: 4 },
+  { card: 'five', val: 5 },
+  { card: 'six', val: 6 },
+  { card: 'seven', val: 7 },
+  { card: 'eight', val: 8 },
+  { card: 'nine', val: 8 },
+  { card: 'ten', val: 10 },
+  { card: 'jack', val: 10 },
+  { card: 'queen', val: 10 },
+  { card: 'king', val: 10 },
+];
+
+console.log('ace equals: ' + club[0].val[1] + ' and ' + club[0].val[0]);
+console.log();
+
+
+//Cards Hearts
+var heart = [
+  { card: 'ace', val: [1, 11] },
+  { card: 'two', val: 2 },
+  { card: 'three', val: 3 },
+  { card: 'four', val: 4 },
+  { card: 'five', val: 5 },
+  { card: 'six', val: 6 },
+  { card: 'seven', val: 7 },
+  { card: 'eight', val: 8 },
+  { card: 'nine', val: 8 },
+  { card: 'ten', val: 10 },
+  { card: 'jack', val: 10 },
+  { card: 'queen', val: 10 },
+  { card: 'king', val: 10 },
+];
+
+//Cards Diamonds
+var diamond = [
+  { card: 'ace', val: [1, 11] },
+  { card: 'two', val: 2 },
+  { card: 'three', val: 3 },
+  { card: 'four', val: 4 },
+  { card: 'five', val: 5 },
+  { card: 'six', val: 6 },
+  { card: 'seven', val: 7 },
+  { card: 'eight', val: 8 },
+  { card: 'nine', val: 8 },
+  { card: 'ten', val: 10 },
+  { card: 'jack', val: 10 },
+  { card: 'queen', val: 10 },
+  { card: 'king', val: 10 },
+];
+
